84a398bd3a901af71c52cf69e541d1e0eec17fe1
Rohitchopade9822 <rohitprofessional99@gmail.com>
2026-07-14 11:53:33 +0530

    get method and dI

--- BatchnewB3/Repo/ProductRepo.cs
+++ BatchnewB3/Repo/ProductRepo.cs
@@ -1,16 +1,42 @@
-﻿using BatchnewB3.Model;
+﻿
+using BatchnewB3.Model;
+using Microsoft.Data.SqlClient;
 
 namespace BatchnewB3.Repo
 {
     public class ProductRepo : IProductRepo
     {
+        private string _connectionString;
+        public ProductRepo(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
         public IEnumerable<Product> GetMethod()
         {
-            //ado.net code
+            List<Product> product= new List<Product>();
+            SqlConnection con = new SqlConnection(_connectionString);
+            string query="select * from Product";
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader reader = cmd.ExecuteReader();
 
-            //DefaultConnection/
+            while(reader.Read())
+            {
 
-            //select * from Product
+                Product prod = new Product()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    order_id = Convert.ToInt32(reader["order_id"]),
+                    Location = reader["Location"].ToString(),
+                    isavailable = Convert.ToBoolean(reader["isavailable"]),
+                    Price = Convert.ToInt32(reader["Price"])
+
+                };
+                product.Add(prod);
+            }
+
+            return product;
         }
         public void AddProuduct(Product product)
         {
